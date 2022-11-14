4e771f5c9594c9a09faafc731d65ac2d86f8399a
Mariazucena Calderòn Riera <mariazucenacalderonriera@gmail.com>
2022-11-14 03:51:06 +0000

    Se agrego estado al componente form.js, además se añadio función para boton +

--- src/form.js
+++ src/form.js
@@ -5,13 +5,23 @@ import React from "react";
 //vamos a recibir la propiedad movie
 //con props
 export default function Form(props) {
+  //vamos a usar el estado de react para
+//que el número tenga información dinamica
+//segun los click del usuario
+//crearemos nuestro componente segun
+//una función NO como una clase
+//creamos una variable que es un array
+//entre () le indicamos que estado debe tener
+const [quantity, setQuantity] = React.useState(0);
   const { movie } = props;
   return (
     <form>
       <h3>{movie.name}</h3>
       <button type='button'> - </button> 
-      0 
-      <button type='button'> + </button>
+     {quantity}
+      <button type='button' onclick = {
+        () => setQuantity(quantity+1)}> + </button>
     </form>
   );
 }
+
